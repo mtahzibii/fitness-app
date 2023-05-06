d5370252a9481ad98e6397bab7438118bb5b1f1e
Tahzibi <m.r.tahzibi@gmail.com>
2023-05-07 00:24:47 +0430

    Update loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Stack } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import { InfinitySpin } from "react-loader-spinner";
+import spinner from "../assets/images/spinner.gif";
 
 const Loader = () => {
   return (
-    <Stack
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      width="100%">
-      <InfinitySpin color="gray" />
-    </Stack>
+    <div className="loader">
+      <img src={spinner} alt="Loading" />
+    </div>
+    // <Stack
+    //   direction="row"
+    //   justifyContent="center"
+    //   alignItems="center"
+    //   width="100%">
+    //   <InfinitySpin color="gray" />
+    // </Stack>
   );
 };
 
