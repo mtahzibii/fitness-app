9047d1484cee60e31e6a7ed99d902e37975bc833
Tahzibi <m.r.tahzibi@gmail.com>
2023-05-07 00:34:21 +0430

    Uninstall react-loader-spinner package

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Stack, Box } from "@mui/material";
-import { InfinitySpin } from "react-loader-spinner";
 import spinner from "../assets/images/spinner.gif";
 
 const Loader = () => {
@@ -8,13 +6,6 @@ const Loader = () => {
     <div className="loader">
       <img src={spinner} alt="Loading" />
     </div>
-    // <Stack
-    //   direction="row"
-    //   justifyContent="center"
-    //   alignItems="center"
-    //   width="100%">
-    //   <InfinitySpin color="gray" />
-    // </Stack>
   );
 };
 
